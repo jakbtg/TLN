9f3a7aa069fc4cc7d357ba0fa20932b53b9b35df
Giacomo Grandi <jak.grandi@gmail.com>
2022-12-28 18:10:27 +0100

    importing spacy

--- Mazzei/src/analysis.py
+++ Mazzei/src/analysis.py
@@ -1,3 +1,8 @@
+import spacy
+
+
 class Analysis:
     def __init__(self, text):
-        self.text = text
+        self.text = text.lower()
+        self.nlp = spacy.load("en_core_web_sm")
+        self.doc = self.nlp(self.text)
