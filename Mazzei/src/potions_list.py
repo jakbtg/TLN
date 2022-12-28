dbde1a521b952870b6097bc194cac29e6403a843
Giacomo Grandi <jak.grandi@gmail.com>
2022-12-28 13:10:37 +0100

    creating a list of potions for the system

---
+++ Mazzei/src/potions_list.py
@@ -0,0 +1,28 @@
+import potion
+
+polyjuice_potion = potion.Potion(
+    "Polyjuice Potion",
+    [
+        "Lacewing Flies",
+        "Leeches",
+        "Powdered Bicorn horn",
+        "Knotgrass",
+        "Fluxweed",
+        "Shredded Boomslang Skin",
+        "Hair",
+    ],
+)
+felix_felicis = potion.Potion(
+    "Felix Felicis",
+    [
+        "Ashwinder egg",
+        "Squill bulb",
+        "Murtlap tentacles",
+        "Tincture of thyme",
+        "Occamy eggshell",
+        "Powdered common rue",
+    ],
+)
+
+print(polyjuice_potion)
+print(felix_felicis)
