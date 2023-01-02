4efb7d4dd7e8bfc17760141284b8757534d9987a
Giacomo Grandi <jak.grandi@gmail.com>
2023-01-02 19:52:52 +0100

    trying with spacy

--- Mazzei/src/analysis.py
+++ Mazzei/src/analysis.py
@@ -1,4 +1,5 @@
 import spacy
+from spacy import displacy
 
 
 class Analysis:
@@ -11,4 +12,10 @@ class Analysis:
 if __name__ == "__main__":
     analysis = Analysis("This is a test")
     print(analysis.text)
+    print(type(analysis.text))
     print(analysis.doc)
+    print(type(analysis.doc))
+    print(analysis.doc[0])
+    print(type(analysis.doc[0]))
+    print(analysis.doc.to_json())
+    displacy.serve(analysis.doc, style="dep", port=8080)
