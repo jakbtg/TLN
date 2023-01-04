f383179c993d60e254a79483537ca4eb543029ed
Giacomo Grandi <jak.grandi@gmail.com>
2023-01-04 17:31:44 +0100

    now trying with markov

--- Mazzei/src/nlg.py
+++ Mazzei/src/nlg.py
@@ -1,23 +1,36 @@
-# Class that generates the text with simpleNLG
-
-from simplenlg import *
+import markovify
 
 
 class NLG:
-    def __init__(self):
-        self.lexicon = Lexicon.getDefaultLexicon()
-        self.nlgFactory = NLGFactory(self.lexicon)
-        self.realiser = Realiser(self.lexicon)
-
-    # Generate a random intro
-    def generate_intro(self):
-        intro = self.nlgFactory.createClause()
-        intro.setSubject("Harry")
-        intro.setVerb("be")
-        intro.setObject("in the potions class")
-        return self.realiser.realiseSentence(intro)
+    def __init__(self, text_path):
+        with open(text_path) as f:
+            text = f.read()
+        self.model = markovify.Text(text)
 
 
 if __name__ == "__main__":
-    nlg = NLG()
-    print(nlg.generate_intro())
+    nlg = NLG("../corpus/questions.txt")
+    print(nlg.model.make_sentence())
+
+
+# from simplenlg import *
+
+
+# class NLG:
+#     def __init__(self):
+#         self.lexicon = Lexicon.getDefaultLexicon()
+#         self.nlgFactory = NLGFactory(self.lexicon)
+#         self.realiser = Realiser(self.lexicon)
+
+#     # Generate a random intro
+#     def generate_intro(self):
+#         intro = self.nlgFactory.createClause()
+#         intro.setSubject("Harry")
+#         intro.setVerb("be")
+#         intro.setObject("in the potions class")
+#         return self.realiser.realiseSentence(intro)
+
+
+# if __name__ == "__main__":
+#     nlg = NLG()
+#     print(nlg.generate_intro())
