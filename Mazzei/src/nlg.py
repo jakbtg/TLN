dec95ae9542027b47f2582c5f031b0277c9085e6
Giacomo Grandi <jak.grandi@gmail.com>
2023-01-05 11:42:05 +0100

    refactor nlg class

--- Mazzei/src/nlg.py
+++ Mazzei/src/nlg.py
@@ -3,26 +3,35 @@ from analysis import Analysis
 
 
 class NLG:
-    def __init__(self, text_path):
-        with open(text_path) as f:
-            text = f.read()
-        self.model = markovify.Text(text)
+    def __init__(self, corpus):
+        self.text = self.build_text_from_corpus(corpus)
+        self.model = markovify.Text(self.text)
         self.model.compile(inplace=True)
+
+    # Choose the corpus to use
+    def build_text_from_corpus(self, corpus):
+        if corpus == "questions":
+            with open("Mazzei/corpus/questions.txt") as f:
+                text = f.read()
+        elif corpus == "answers":
+            with open("Mazzei/corpus/answers.txt") as f:
+                text = f.read()
+        return text
 
     def ask_question(self):
         question = self.model.make_sentence(tries=100)
-        text = Analysis(question)
+        test = Analysis(question)
         # Make sure the question contains only one ingredient
-        while text.number_of_ingredients() >= 2 or text.number_of_ingredients() == 0:
+        while test.number_of_ingredients() >= 2 or test.number_of_ingredients() == 0:
             question = self.model.make_sentence(tries=100)
-            text = Analysis(question)
+            test = Analysis(question)
         return question
 
 
 if __name__ == "__main__":
-    nlg = NLG("Mazzei/corpus/questions.txt")
+    questions_generator = NLG("questions")
     for i in range(10):
-        print(nlg.ask_question())
+        print(questions_generator.ask_question())
 
 
 # from simplenlg import *
