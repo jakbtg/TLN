062fb47c59b5ec3a9811cabaa6861826ef9202d1
Giacomo Grandi <jak.grandi@gmail.com>
2023-01-09 18:13:01 +0100

    removed useless simplenlg comments

--- Mazzei/src/nlg.py
+++ Mazzei/src/nlg.py
@@ -81,26 +81,3 @@ if __name__ == "__main__":
     neg_answers_generator = NLG("negative answers")
     for i in range(20):
         print(neg_answers_generator.generate_answer())
-
-
-# from simplenlg import *
-
-
-# class NLG:
-#     def __init__(self):
-#         self.lexicon = Lexicon.getDefaultLexicon()
-#         self.nlgFactory = NLGFactory(self.lexicon)
-#         self.realiser = Realiser(self.lexicon)
-
-#     # Generate a random intro
-#     def generate_intro(self):
-#         intro = self.nlgFactory.createClause()
-#         intro.setSubject("Harry")
-#         intro.setVerb("be")
-#         intro.setObject("in the potions class")
-#         return self.realiser.realiseSentence(intro)
-
-
-# if __name__ == "__main__":
-#     nlg = NLG()
-#     print(nlg.generate_intro())
